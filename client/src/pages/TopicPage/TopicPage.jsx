df8b5ef6944418ed5dae22589e1703f9248f9e2d
«aselya93» <asel_hbk@mail.ru>
2024-11-29 13:48:07 +0300

    quiz

--- client/src/pages/TopicPage/TopicPage.jsx
+++ client/src/pages/TopicPage/TopicPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+
 import Button from '../../shared/ui/Button/Button'
 import { Link, useNavigate } from 'react-router-dom';
+import { message as antMessage } from 'antd'
+import QuizTopicCard from '../../widgets/QuizTopicCard/QuizTopicCard';
 
 
 function TopicPage() {
@@ -9,6 +11,7 @@ function TopicPage() {
         <div>
             <h1>Выбираем тему викторины</h1>
             <Link to="/quiz">
+            < QuizTopicCard />
                 <Button text="Какая-то тема" Перейти к викторине onClick={() => navigate('/quiz')} />
             </Link>
         </div>
---
+++ client/src/widgets/QuizQuestionForm/QuizQuestionForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QuizQuestionForm(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default QuizQuestionForm;
